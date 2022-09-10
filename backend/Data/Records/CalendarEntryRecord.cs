f34ba925f0c231d3d61fd6cbe9a974467a199ac8
wjbaker101 <willbaker10198@gmail.com>
2022-09-10 12:57:50 +0100

    Create record for calendar entries

---
+++ backend/Data/Records/CalendarEntryRecord.cs
@@ -0,0 +1,39 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Data.Records;
+
+public class CalendarEntryRecord
+{
+    public virtual long Id { get; init; }
+    public virtual Guid Reference { get; init; }
+    public virtual DateTime CreatedAt { get; init; }
+    public virtual string Description { get; set; } = null!;
+    public virtual DateTime StartAt { get; set; }
+    public virtual DateTime EndAt { get; set; }
+    public virtual CalendarEntryRecurringPeriod RecurringPeriod { get; set; }
+}
+
+public enum CalendarEntryRecurringPeriod
+{
+    Unknown = 0,
+    None = 1,
+    Weekly = 2,
+    Monthly = 3,
+    Yearly = 4
+}
+
+public sealed class CalendarEntryRecordMap : ClassMap<CalendarEntryRecord>
+{
+    public CalendarEntryRecordMap()
+    {
+        Schema("calendar");
+        Table("entry");
+        Id(x => x.Id, "id").GeneratedBy.SequenceIdentity("entry_id_seq");
+        Map(x => x.Reference, "reference");
+        Map(x => x.CreatedAt, "created_at");
+        Map(x => x.Description, "description");
+        Map(x => x.StartAt, "start_at");
+        Map(x => x.EndAt, "end_at");
+        Map(x => x.RecurringPeriod, "recurring_period").CustomType<CalendarEntryRecurringPeriod>();
+    }
+}
