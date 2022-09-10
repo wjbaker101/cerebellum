3f9bacb6489b0687170ae1eefd4bc00fbaaa0da6
wjbaker101 <willbaker10198@gmail.com>
2022-09-10 13:24:43 +0100

    Rename and add logic for ApiDatabase

---
+++ backend/Data/ApiDatabase.cs
@@ -0,0 +1,28 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace Data;
+
+public interface IApiDatabase
+{
+    ISessionFactory SessionFactory { get; }
+}
+
+public sealed class ApiDatabase : IApiDatabase
+{
+    public ISessionFactory SessionFactory { get; }
+
+    public ApiDatabase()
+    {
+        SessionFactory = Fluently.Configure()
+            .Database(PostgreSQLConfiguration.Standard.ConnectionString(c => c
+                .Host("localhost")
+                .Port(5432)
+                .Database("cerebellum")
+                .Username("postgres")
+                .Password("password")))
+            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ApiDatabase>())
+            .BuildSessionFactory();
+    }
+}
--- backend/Data/AppDatabase.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Data;
-
-public sealed class AppDatabase
-{
-    
-}
