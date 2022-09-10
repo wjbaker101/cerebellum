fa838dac65acb585398c00876ab277f87e0805e1
wjbaker101 <willbaker10198@gmail.com>
2022-09-10 12:59:42 +0100

    Create model fror calendar entries

---
+++ backend/Core/Model/CalendarEntryModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Model;
+
+public sealed class CalendarEntryModel
+{
+    public Guid Reference { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public string Description { get; init; } = null!;
+    public DateTime StartAt { get; init; }
+    public DateTime EndAt { get; init; }
+    public CalendarEntryRecurringPeriod RecurringPeriod { get; init; }
+}
+
+public enum CalendarEntryRecurringPeriod
+{
+    Unknown = 0,
+    None = 1,
+    Weekly = 2,
+    Monthly = 3,
+    Yearly = 4
+}
