f828c9ea4c46f2187b85f010a3968dc5e7d3dacd
wjbaker101 <willbaker10198@gmail.com>
2022-09-10 22:16:11 +0100

    Create a helper for queries

---
+++ backend/Data.Tests/_Helper/DatabaseHelper.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NHibernate;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Data.Tests._Helper;
+
+public static class DatabaseHelper
+{
+    private static readonly Mock<ISession> Session;
+    private static readonly Mock<IApiDatabase> Database;
+
+    static DatabaseHelper()
+    {
+        Session = new Mock<ISession>();
+        Session
+            .Setup(mock => mock.BeginTransaction())
+            .Returns(Moq.Mock.Of<ITransaction>());
+
+        Database = new Mock<IApiDatabase>();
+        Database
+            .Setup(mock => mock.SessionFactory.OpenSession())
+            .Returns(Session.Object);
+    }
+
+    public static Mock<IApiDatabase> Mock<T>(List<T> records)
+    {
+        Session
+            .Setup(mock => mock.Query<T>())
+            .Returns(records.AsQueryable());
+
+        return Database;
+    }
+}
