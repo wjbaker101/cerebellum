13bc6bebd42e31ce063d508a98761415117826e2
wjbaker101 <willbaker10198@gmail.com>
2022-09-10 13:25:13 +0100

    Add ApiDatabase as injectable dep

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -1,10 +1,13 @@
 using Api.Calendar;
+using Data;
 using Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddControllers();
+
+services.AddSingleton<IApiDatabase, ApiDatabase>();
 
 services.AddSingleton<ICalendarService, CalendarService>();
 services.AddSingleton<ICalendarRepository, CalendarRepository>();
