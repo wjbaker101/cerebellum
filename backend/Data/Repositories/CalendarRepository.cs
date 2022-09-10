1592318d3c2b5e02c7ffce2783bcd2f73fd4a6e6
wjbaker101 <willbaker10198@gmail.com>
2022-09-10 13:30:54 +0100

    Add functionality to get a calendar entry

--- backend/Data/Repositories/CalendarRepository.cs
+++ backend/Data/Repositories/CalendarRepository.cs
@@ -1,12 +1,36 @@
-﻿namespace Data.Repositories;
+﻿using Data.Records;
+using NetApiLibs.Type;
+
+namespace Data.Repositories;
 
 public interface ICalendarRepository
 {
+    Result<CalendarEntryRecord> GetEntryByReference(Guid reference);
 }
 
 public sealed class CalendarRepository : ICalendarRepository
 {
-    public CalendarRepository()
+    private readonly IApiDatabase _database;
+
+    public CalendarRepository(IApiDatabase database)
     {
+        _database = database;
+    }
+
+    public Result<CalendarEntryRecord> GetEntryByReference(Guid reference)
+    {
+        using var session = _database.SessionFactory.OpenSession();
+        using var transaction = session.BeginTransaction();
+
+        var entry = session
+            .Query<CalendarEntryRecord>()
+            .SingleOrDefault();
+
+        transaction.Commit();
+
+        if (entry == null)
+            return Result<CalendarEntryRecord>.Failure($"Unable to find calendar entry with given reference: {reference}.");
+
+        return entry;
     }
 }
