bf6ee734872eb063c7694cc90d8a935253cbabf6
wjbaker101 <willbaker10198@gmail.com>
2022-09-22 19:26:01 +0100

    Create note record

---
+++ backend/Data/Records/NoteRecord.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Data.Records;
+
+public class NoteRecord
+{
+    public virtual long Id { get; init; }
+    public virtual Guid Reference { get; init; }
+    public virtual DateTime CreatedAt { get; init; }
+    public virtual string Content { get; set; } = null!;
+}
+
+public sealed class NoteRecordMap : ClassMap<NoteRecord>
+{
+    public NoteRecordMap()
+    {
+        Schema("notes");
+        Table("note");
+        Id(x => x.Id, "id").GeneratedBy.SequenceIdentity("note_id_seq");
+        Map(x => x.Reference, "reference");
+        Map(x => x.CreatedAt, "created_at");
+        Map(x => x.Content, "content");
+    }
+}
