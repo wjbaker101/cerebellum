08af8d7ccb8aa0a9cb477a23719520a4543908e8
wjbaker101 <willbaker10198@gmail.com>
2022-09-22 22:31:30 +0100

    List notes

--- frontend/src/view/notes/NotesView.vue
+++ frontend/src/view/notes/NotesView.vue
@@ -1,10 +1,33 @@
 <template>
     <ViewComponent class="notes-view" heading="Notes" :no-header="false">
-        asd
+        <div v-if="notes === null">
+            <p>Loading...</p>
+        </div>
+        <div v-else>
+            <router-link class="note" :to="`/notes/${note.reference}`" :key="note.reference" v-for="note in notes">
+                <div>
+                    {{ note.reference }}
+                </div>
+            </router-link>
+        </div>
     </ViewComponent>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+import { useApi } from '@/use/api/api.use';
+import { INote } from '@/model/Note.model';
+
+const api = useApi();
+
+const notes = ref<Array<INote> | null>(null);
+
+onMounted(async () => {
+    const response = await api.notes.search();
+
+    notes.value = response;
+});
 </script>
 
 <style lang="scss">
