8b812e56eb7fcf0eaa10990d7a91b9c8263bbe02
wjbaker101 <willbaker10198@gmail.com>
2022-09-22 19:29:29 +0100

    Create notes repository

---
+++ backend/Data/Repositories/NotesRepository.cs
@@ -0,0 +1,36 @@
+﻿using Data.Records;
+using NetApiLibs.Type;
+
+namespace Data.Repositories;
+
+public interface INotesRepository
+{
+    Result<NoteRecord> GetByReference(Guid reference);
+}
+
+public sealed class NotesRepository : BaseRepository, INotesRepository
+{
+    private readonly IApiDatabase _database;
+
+    public NotesRepository(IApiDatabase database) : base(database)
+    {
+        _database = database;
+    }
+
+    public Result<NoteRecord> GetByReference(Guid reference)
+    {
+        using var session = _database.SessionFactory.OpenSession();
+        using var transaction = session.BeginTransaction();
+
+        var note = session
+            .Query<NoteRecord>()
+            .SingleOrDefault(x => x.Reference == reference);
+
+        transaction.Commit();
+
+        if (note == null)
+            return Result<NoteRecord>.Failure($"Unable to find note with given reference: {reference}.");
+
+        return note;
+    }
+}
