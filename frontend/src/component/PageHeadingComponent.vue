5130ecf12ec28c510cd3ed2523aa493c87e4e0ce
wjbaker101 <willbaker10198@gmail.com>
2022-09-24 19:54:25 +0100

    Add gradient border to pageHeadingComponent

--- frontend/src/component/PageHeadingComponent.vue
+++ frontend/src/component/PageHeadingComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="page-heading-component flex flex-auto align-items-center gap" v-if="!noHeader">
+    <GradientBorderComponent class="page-heading-component flex flex-auto align-items-center gap" v-if="!noHeader">
         <div>
             <slot name="title">
                 <h1>{{ heading }}</h1>
@@ -8,10 +8,12 @@
         <div class="flex-auto">
             <slot></slot>
         </div>
-    </div>
+    </GradientBorderComponent>
 </template>
 
 <script setup lang="ts">
+import GradientBorderComponent from '@/component/GradientBorderComponent.vue';
+
 defineProps<{
     heading: string;
     noHeader?: boolean;
@@ -21,8 +23,6 @@ defineProps<{
 <style lang="scss">
 .page-heading-component {
     padding: 1rem;
-    border: 2px solid var(--wjb-primary);
-    border-radius: var(--wjb-border-radius);
 
     h1 {
         margin: 0;
