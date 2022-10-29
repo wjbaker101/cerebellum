41b1408e6e818966a7fe1248bb082e3489dd94a5
wjbaker101 <willbaker10198@gmail.com>
2022-10-29 20:48:51 +0100

    Create ListumController

---
+++ backend/Api/Listum/ListumController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetApiLibs.Api;
+
+namespace Api.Listum;
+
+[Route("api/listum")]
+public sealed class ListumController : ApiController
+{
+    private readonly IListumService _listumService;
+
+    public ListumController(IListumService listumService)
+    {
+        _listumService = listumService;
+    }
+
+    [HttpGet]
+    [Route("list/{reference:guid}")]
+    public IActionResult GetListByReference([FromRoute] Guid reference)
+    {
+        var result = _listumService.GetListByReference(reference);
+
+        return ToApiResponse(result);
+    }
+}
