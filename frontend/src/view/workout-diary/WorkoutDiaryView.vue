334972436e03d8ab48ebcb4d6df584ddc2236f55
wjbaker101 <willbaker10198@gmail.com>
2022-11-23 23:14:05 +0000

    Show modal when adding a new entry

--- frontend/src/view/workout-diary/WorkoutDiaryView.vue
+++ frontend/src/view/workout-diary/WorkoutDiaryView.vue
@@ -1,7 +1,7 @@
 <template>
     <ViewComponent class="workout-diary-view flex flex-vertical gap" heading="Workout Diary">
         <template v-slot:header>
-            <ButtonComponent class="primary">
+            <ButtonComponent class="primary" @click="onAddEntry">
                 <IconComponent icon="plus" gap="right" />
                 <span>New Entry</span>
             </ButtonComponent>
@@ -16,12 +16,15 @@
 import { onMounted, ref } from 'vue';
 
 import WorkoutEntryComponent from '@/view/workout-diary/component/WorkoutEntry.component.vue';
+import WorkoutEntryModalComponent from '@/view/workout-diary/modal/WorkoutEntry.modal.component.vue';
 
+import { useModal } from '@wjb/vue/use/modal.use';
 import { useApi } from '@/use/api/api.use';
 
 import { IWorkoutEntry } from './model/WorkoutEntry.model';
 
 const api = useApi();
+const modal = useModal();
 
 const entries = ref<Array<IWorkoutEntry> | null>(null);
 
@@ -30,6 +33,13 @@ onMounted(async () => {
 
     entries.value = result;
 });
+
+const onAddEntry = function (): void {
+    modal.show({
+        component: WorkoutEntryModalComponent,
+        componentProps: {},
+    });
+};
 </script>
 
 <style lang="scss">
