3040f2b690ff13eda464cf081c584727a0a8ab01
wjbaker101 <willbaker10198@gmail.com>
2022-11-25 19:04:32 +0000

    Register popupComponent

--- frontend/src/app/setup/components.ts
+++ frontend/src/app/setup/components.ts
@@ -6,6 +6,8 @@ import FormComponent from '@wjb/vue/component/form/FormComponent.vue';
 import FormInputComponent from '@wjb/vue/component/form/FormInputComponent.vue';
 import FormSectionComponent from '@wjb/vue/component/form/FormSectionComponent.vue';
 import IconComponent from '@wjb/vue/component/IconComponent.vue';
+import PopupComponent from '@wjb/vue/component/PopupComponent.vue';
+
 import ViewComponent from '@/component/ViewComponent.vue';
 
 export const components: Plugin = {
@@ -16,6 +18,8 @@ export const components: Plugin = {
         app.component('FormInputComponent', FormInputComponent);
         app.component('FormSectionComponent', FormSectionComponent);
         app.component('IconComponent', IconComponent);
+        app.component('PopupComponent', PopupComponent);
+
         app.component('ViewComponent', ViewComponent);
     },
 };
