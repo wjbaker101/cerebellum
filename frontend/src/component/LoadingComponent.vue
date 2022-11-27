ed9fe03d394ad7e397bd8a99879d539a806492c7
wjbaker101 <willbaker10198@gmail.com>
2022-11-27 18:51:43 +0000

    Create components for pages that load content

---
+++ frontend/src/component/LoadingComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="loading-component text-centered">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid"
+        >
+            <circle cx="50" cy="50" fill="none" stroke="currentColor" stroke-width="5" r="47" stroke-dasharray="115 80" stroke-linecap="round">
+                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1" />
+            </circle>
+        </svg>
+        <p>Loading {{ itemName }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    itemName?: string;
+}>();
+</script>
+
+<style lang="scss">
+.loading-component {
+    margin: 2rem;
+
+    svg {
+        $size: 70px;
+
+        width: $size;
+        height: $size;
+        color: var(--wjb-primary);
+    }
+}
+</style>
---
+++ frontend/src/component/NoItemsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="no-items-component">
+        <IconComponent icon="bin" size="large" />
+        <p>No {{ displayItemName }} yet!</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    itemName?: string;
+}>();
+
+const displayItemName = props.itemName ?? 'items';
+</script>
+
+<style lang="scss">
+</style>
