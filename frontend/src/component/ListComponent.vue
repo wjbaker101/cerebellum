799627f6015a040f3bb6f3a7a75c0238e23fd5db
wjbaker101 <willbaker10198@gmail.com>
2022-12-09 13:28:49 +0000

    Create a generic list component for views that list items that redirect to a feature

---
+++ frontend/src/component/ListComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <LoadingComponent :itemName="itemName" v-if="items === null" />
+    <NoItemsComponent :itemName="itemName" v-else-if="items.length === 0" />
+    <ul v-else class="list-component">
+        <template v-for="item in items">
+            <router-link v-if="urlMapper" :to="urlMapper(item)">
+                <slot name="item" :item="item"></slot>
+            </router-link>
+            <li v-else>
+                <slot name="item" :item="item"></slot>
+            </li>
+        </template>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import LoadingComponent from '@/component/LoadingComponent.vue';
+import NoItemsComponent from '@/component/NoItemsComponent.vue';
+
+type ItemType = any;
+
+defineProps<{
+    items: Array<ItemType> | null;
+    itemName?: string;
+    urlMapper?: (x: ItemType) => string;
+}>();
+</script>
+
+<style lang="scss">
+.list-component {
+    width: 720px;
+    max-width: 100%;
+    margin: 1rem auto 1rem auto;
+    padding-left: 0;
+    list-style: none;
+
+    & > a,
+    & > li {
+        display: block;
+        padding: 0.5rem;
+        border: 2px solid var(--wjb-background-colour-dark);
+        border-radius: var(--wjb-border-radius);
+        color: inherit;
+        text-decoration: none;
+
+        & + a,
+        & + li {
+            margin-top: 0.5rem;
+        }
+    }
+}
+</style>
