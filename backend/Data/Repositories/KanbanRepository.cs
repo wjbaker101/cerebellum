812f34d56759b4cfefa7706aa873755041e953a9
wjbaker101 <willbaker10198@gmail.com>
2022-12-11 14:40:14 +0000

    Create method for getting a kanban board by it's reference

--- backend/Data/Repositories/KanbanRepository.cs
+++ backend/Data/Repositories/KanbanRepository.cs
@@ -1,12 +1,41 @@
-﻿namespace Data.Repositories;
+﻿using Data.Records;
+using NetApiLibs.Type;
+using NHibernate.Linq;
+
+namespace Data.Repositories;
 
 public interface IKanbanRepository
 {
+    Result<KanbanBoardRecord> GetBoard(Guid reference);
 }
 
 public sealed class KanbanRepository : BaseRepository, IKanbanRepository
 {
     public KanbanRepository(IApiDatabase database) : base(database)
     {
+    }
+
+    public Result<KanbanBoardRecord> GetBoard(Guid reference)
+    {
+        using var session = Database.SessionFactory.OpenSession();
+        using var transaction = session.BeginTransaction();
+
+        var kanbanBoard = session
+            .Query<KanbanBoardRecord>()
+            .FetchMany(x => x.Columns)
+            .SingleOrDefault(x => x.Reference == reference);
+
+        session
+            .Query<KanbanColumnRecord>()
+            .FetchMany(x => x.Items)
+            .Where(x => x.Board.Reference == reference)
+            .ToFuture();
+
+        transaction.Commit();
+
+        if (kanbanBoard == null)
+            return Result<KanbanBoardRecord>.Failure($"Kanban board '{reference}' could not be found.");
+
+        return kanbanBoard;
     }
 }
