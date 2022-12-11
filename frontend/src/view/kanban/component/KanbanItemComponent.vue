486d0f8ac44cfe7a2c761837da8e6bedb74d590c
wjbaker101 <willbaker10198@gmail.com>
2022-12-11 11:42:49 +0000

    Add modal when clicking kanban items

--- frontend/src/view/kanban/component/KanbanItemComponent.vue
+++ frontend/src/view/kanban/component/KanbanItemComponent.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="kanban-item-component draggable">
+    <div class="kanban-item-component draggable" @click="onClick">
         {{ kanbanItem.content }}
     </div>
 </template>
 
 <script setup lang="ts">
-import { IKanbanItem } from '@/view/kanban/model/KanbanBoard.model';
+import { useModal } from '@wjb/vue/use/modal.use';
 
-defineProps<{
+import { IKanbanItem } from '@/view/kanban/model/KanbanBoard.model';
+import KanbanItemModalComponent from '@/view/kanban/modal/KanbanItem.modal.component.vue';
+
+const props = defineProps<{
     kanbanItem: IKanbanItem;
 }>();
+
+const modal = useModal();
+
+const onClick = function (): void {
+    modal.show({
+        component: KanbanItemModalComponent,
+        componentProps: {
+            kanbanItem: props.kanbanItem,
+        },
+    });
+};
 </script>
 
 <style lang="scss">
---
+++ frontend/src/view/kanban/modal/KanbanItem.modal.component.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="kanban-item-modal-component">
+        <h2>Edit Item</h2>
+        <FormComponent>
+            <FormSectionComponent>
+                <FormInputComponent label="Content">
+                    <input type="text" v-model="form.content">
+                </FormInputComponent>
+            </FormSectionComponent>
+            <FormSectionComponent class="flex gap-small">
+                <ButtonComponent class="primary flex-auto" @click="onConfirm">
+                    <IconComponent icon="tick" gap="right" />
+                    <span>Confirm</span>
+                </ButtonComponent>
+                <DeleteButtonComponent class="flex-auto" />
+            </FormSectionComponent>
+        </FormComponent>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { IKanbanItem } from '@/view/kanban/model/KanbanBoard.model';
+
+const props = defineProps<{
+    kanbanItem: IKanbanItem;
+}>();
+
+interface IKanbanItemForm {
+    content: string;
+}
+
+const form = ref<IKanbanItemForm>({
+    content: props.kanbanItem.content,
+});
+
+const onConfirm = function (): void {
+    props.kanbanItem.content = form.value.content;
+};
+</script>
+
+<style lang="scss">
+.kanban-item-modal-component {
+    width: 720px;
+    max-width: 100%;
+}
+</style>
