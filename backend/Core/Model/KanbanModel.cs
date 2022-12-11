2459a56d42015764cd9da1e3a77b054995197096
wjbaker101 <willbaker10198@gmail.com>
2022-12-11 14:28:34 +0000

    Add model for kanban

---
+++ backend/Core/Model/KanbanModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Model;
+
+public sealed class KanbanBoardModel
+{
+    public Guid Reference { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public string Title { get; init; } = null!;
+    public List<KanbanColumnModel> Columns { get; init; } = new();
+}
+
+public sealed class KanbanColumnModel
+{
+    public Guid Reference { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public string Title { get; init; } = null!;
+    public List<KanbanItemModel> Items { get; init; } = new();
+}
+
+public sealed class KanbanItemModel
+{
+    public Guid Reference { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public string Content { get; init; } = null!;
+}
