77c92bce58f30c615d4ce273873fd5819515205d
wjbaker101 <willbaker10198@gmail.com>
2023-03-25 18:17:34 +0000

    Create a testHelpers test project

---
+++ backend/Tests/TestHelpers/Fakes/FakeNotesRepository.cs
@@ -0,0 +1,52 @@
+﻿using Data.Records;
+using Data.Repositories;
+using NetApiLibs.Type;
+
+namespace TestHelpers.Fakes;
+
+public sealed class FakeNotesRepository : INotesRepository
+{
+    public NoteRecord? ActionedNote { get; private set; }
+
+    private readonly Result<NoteRecord> _noteResult;
+
+    private FakeNotesRepository(Result<NoteRecord> noteResult)
+    {
+        _noteResult = noteResult;
+    }
+
+    public static FakeNotesRepository Default() => new(new NoteRecord
+    {
+        Reference = Guid.Parse("8be11292-8788-4a8a-b3e4-93835086a4b2"),
+        CreatedAt = new DateTime(2022, 05, 14, 00, 05, 42),
+        Title = "TestTitle",
+        Content = "TestContent"
+    });
+
+    public static FakeNotesRepository Failure(Result<NoteRecord> failure) => new(failure);
+
+    public NoteRecord SaveNote(NoteRecord note)
+    {
+        return ActionedNote = note;
+    }
+
+    public NoteRecord UpdateNote(NoteRecord note)
+    {
+        return ActionedNote = note;
+    }
+
+    public List<NoteRecord> SearchNotes()
+    {
+        return new List<NoteRecord> { _noteResult.Value };
+    }
+
+    public Result<NoteRecord> GetByReference(Guid reference)
+    {
+        return _noteResult.Value;
+    }
+
+    public void DeleteNote(NoteRecord note)
+    {
+        ActionedNote = note;
+    }
+}
