ee430d7beb6a13377430275cc8faba6bbf029307
wjbaker101 <willbaker10198@gmail.com>
2023-04-23 12:35:04 +0100

    Use components setup from lib

--- frontend/src/app/setup/components.ts
+++ frontend/src/app/setup/components.ts
@@ -1,23 +1,15 @@
 import { App, Plugin } from 'vue';
 
-import ButtonComponent from '@wjb/vue/component/ButtonComponent.vue';
-import DeleteButtonComponent from '@wjb/vue/component/DeleteButtonComponent.vue';
-import FormComponent from '@wjb/vue/component/form/FormComponent.vue';
-import FormInputComponent from '@wjb/vue/component/form/FormInputComponent.vue';
-import FormSectionComponent from '@wjb/vue/component/form/FormSectionComponent.vue';
-import IconComponent from '@wjb/vue/component/IconComponent.vue';
+import { components as wjbComponents } from '@wjb/vue/setup/components';
 
 import ViewComponent from '@/component/ViewComponent.vue';
 
 export const components: Plugin = {
+
     install(app: App) {
-        app.component('ButtonComponent', ButtonComponent);
-        app.component('DeleteButtonComponent', DeleteButtonComponent);
-        app.component('FormComponent', FormComponent);
-        app.component('FormInputComponent', FormInputComponent);
-        app.component('FormSectionComponent', FormSectionComponent);
-        app.component('IconComponent', IconComponent);
+        app.use(wjbComponents);
 
         app.component('ViewComponent', ViewComponent);
     },
+
 };
