1b7afc9b04e65f22c96a826ef89ba8de6053da92
William Baker <wjbaker101@users.noreply.github.com>
2024-02-10 20:49:13 +0000

    Remove newline

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,7 +7,6 @@ import App from '@/App.vue';
 import { components } from '@/setup/components';
 import { router } from '@/setup/router';
 
-
 const app = createApp(App);
 app.use(components);
 app.use(router);
