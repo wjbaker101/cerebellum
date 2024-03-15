79d89cee24f949f4716c57a398879e0cccbc452b
William Baker <wjbaker101@users.noreply.github.com>
2024-03-15 23:03:29 +0000

    Add icons for pwa usage

--- frontend/src/vite/pwa.ts
+++ frontend/src/vite/pwa.ts
@@ -1,4 +1,3 @@
-
 import { VitePWA } from 'vite-plugin-pwa';
 
 export const pwa = function () {
@@ -11,6 +10,18 @@ export const pwa = function () {
             short_name: 'Cerebellum',
             description: 'Your mind in digital form.',
             theme_color: '#4db87b',
+            icons: [
+                {
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+            ],
         },
 
     });
